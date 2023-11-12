3b24e49fa00a1864e5a295939b3dddbeb64c10b4
Natthanon Tairattanapong <ntairatt@z1t4c3.42bangkok.com>
2023-11-12 14:47:01 +0700

    Update Format

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <cctype>
 
+void	print_upper(char *av)
+{
+	for (int j = 0; av[j]; j++)
+		std::cout << (char)toupper(av[j]);
+}
+
 int	main(int ac, char **av)
 {
-	if (ac == 1)
+	if (ac < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 		for (int i = 1; av[i]; i++)
-			for (int j = 0; av[i][j]; j++)
-				std::cout << (char)toupper(av[i][j]);
+			print_upper(av[i]);
 	std::cout << std::endl;
 	return (EXIT_SUCCESS);
 }
