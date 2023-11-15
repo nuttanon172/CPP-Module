5b51f4ffe7c200d56a0448c07a15e08231b89d61
Natthanon Tairattanapong <ntairatt@z1t5c1.42bangkok.com>
2023-11-15 12:27:55 +0700

    Update ADD SEARCh

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -7,18 +7,20 @@ int	main(void)
 
 	while (1)
 	{
-		std::cout << "Enter Command: ";
-		std::getline(std::cin, str);
 		if (std::cin.eof())
 		{
 			std::cout << std::endl;
 			return (EXIT_SUCCESS);
 		}
-		else if (!str.compare("ADD"))
+		std::cout << "Enter Command: ";
+		std::getline(std::cin, str);
+		if (!str.compare("ADD"))
 			pb.addContact();
 		else if (!str.compare("SEARCH"))
 			pb.printHeader();
 		else if (!str.compare("EXIT"))
 			return (EXIT_SUCCESS);
+		else
+			std::cout << "Command not found.." << std::endl;
 	}
 }
