ed0e288027ee4a2790af5160e029e705e8da2404
Natthanon Tairattanapong <ntairatt@z1t5c1.42bangkok.com>
2023-11-15 12:32:25 +0700

    Revert "Update ADD SEARCh"
    This reverts commit 23ee6869507959de0cd52fd13ca56f9727a93bd0.

--- CPP00/ex01/contact.hpp
+++ CPP00/ex01/contact.hpp
@@ -5,22 +5,19 @@
 
 class Contact
 {
-	private:
-		std::string	firstname;
-		std::string	lastname;
-		std::string	nickname;
-		std::string	phone_number;
-		std::string	dark_secret;
-
 	public:
-		Contact(void);
-		~Contact(void);
 		void		setValue(std::string f, std::string l, std::string n, std::string p, std::string d);
 		std::string	getFirstN(void);
 		std::string	getLastN(void);
 		std::string	getNickN(void);
 		std::string	getPhoneN(void);
 		std::string	getDarks(void);
+	private:
+		std::string	firstname;
+		std::string	lastname;
+		std::string	nickname;
+		std::string	phone_number;
+		std::string	dark_secret;
 };
 
 #endif
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -7,20 +7,18 @@ int	main(void)
 
 	while (1)
 	{
+		std::cout << "Enter Command: ";
+		std::getline(std::cin, str);
 		if (std::cin.eof())
 		{
 			std::cout << std::endl;
 			return (EXIT_SUCCESS);
 		}
-		std::cout << "Enter Command: ";
-		std::getline(std::cin, str);
-		if (!str.compare("ADD"))
+		else if (!str.compare("ADD"))
 			pb.addContact();
 		else if (!str.compare("SEARCH"))
 			pb.printHeader();
 		else if (!str.compare("EXIT"))
 			return (EXIT_SUCCESS);
-		else
-			std::cout << "Command not found.." << std::endl;
 	}
 }
