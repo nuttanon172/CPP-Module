09b704b22f10136791abc5fbfaa18bed5a2f0c21
Natthanon Tairattanapong <nuttanon172@gmail.com>
2024-05-13 15:34:35 +0700

    Update Indentation

--- CPP05/ex03/Bureaucrat.hpp
+++ CPP05/ex03/Bureaucrat.hpp
@@ -8,31 +8,31 @@ class AForm;
 
 class Bureaucrat
 {
-    private:
-        const std::string _name;
-        int _grade;
-    public:
-        Bureaucrat();
-        Bureaucrat(const std::string &name, const int &grade);
-        ~Bureaucrat();
-        Bureaucrat(const Bureaucrat &obj);
-        Bureaucrat &operator=(const Bureaucrat &obj);
-        const std::string &getName() const;
-        int getGrade() const;
-        void incGrade();
-        void decGrade();
-        void signForm(AForm &obj);
-        void executeForm(const AForm &form);
-    class GradeTooHighExeception : public std::exception
-    {
-        public:
-            virtual const char* what() const throw();
-    };
-    class GradeTooLowExeception : public std::exception
-    {
-        public:
-            virtual const char* what() const throw();
-    };
+	private:
+		const std::string _name;
+		int _grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string &name, const int &grade);
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &obj);
+		Bureaucrat &operator=(const Bureaucrat &obj);
+		const std::string &getName() const;
+		int getGrade() const;
+		void incGrade();
+		void decGrade();
+		void signForm(AForm &obj);
+		void executeForm(const AForm &form);
+	class GradeTooHighExeception : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
+	class GradeTooLowExeception : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
 };
 
 std::ostream&	operator<<(std::ostream& os, const Bureaucrat& obj);
